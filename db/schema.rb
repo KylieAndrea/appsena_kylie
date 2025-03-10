5e43000491e3c52a140b3c2653ad9aa5e5ad8586
andrw790 <168228628+KylieAndrea@users.noreply.github.com>
2025-03-09 19:53:04 -0500

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_07_090748) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_09_224018) do
+  create_table "carros", force: :cascade do |t|
+    t.string "modelo"
+    t.string "placa"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "nombre"
     t.string "tipo_documento"
